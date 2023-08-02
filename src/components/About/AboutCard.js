06caad085f9a05675f90a7235088b0a7544a0e24
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-08-02 17:47:29 +0300

    Remove typo

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hola, I am <span className="purple">Kagema Njoroge </span>
-            from <span className="purple">Nairobi Kenya</span>
+            from <span className="purple">Nairobi, Kenya</span>
             <br /> I am a 2nd year student pursuing BSC Computer Science at the University of Nairobi.
             <br />
             Additionally, I am currently working on Tomorrow, a SaaS platform that helps businesses manage their inventory, sales, customers and tax compliance.
